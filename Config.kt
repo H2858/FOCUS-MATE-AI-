9e401ef4c24c5e4af4346e712913768130fe363d
H2858 <hichembekhedda17@gmail.com>
2026-07-15 22:33:14 +0100

    Delete Config.kt

--- Config.kt
+++
@@ -1,7 +0,0 @@
-object Config {
-    val SUPABASE_URL = System.getenv("SUPABASE_URL") ?: ""
-    val SUPABASE_ANON_KEY = System.getenv("SUPABASE_ANON_KEY") ?: ""
-    val SUPABASE_SERVICE_ROLE_KEY = System.getenv("SUPABASE_SERVICE_ROLE_KEY") ?: ""
-    val ELEVENLABS_API_KEY = System.getenv("ELEVENLABS_API_KEY") ?: ""
-}
-
